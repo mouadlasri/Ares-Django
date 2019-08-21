427d79c12569422c2126fd6f1069bb2e6e02aa3f
Mouad <lasri.mouad98@gmail.com>
2019-08-21 23:56:46 +0100

    Added Azure host

--- Ares/settings.py
+++ Ares/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+PROJECT_DIR = os.path.dirname(__file__)
 
 STATIC_URL = '/static/'
-PROJECT_DIR = 'https://projectares.scm.azurewebsites.net/'
-# STATIC_ROOT = "https://projectares.scm.azurewebsites.net/static/"
 STATIC_ROOT= os.path.join(PROJECT_DIR,'static/')
+# STATIC_ROOT = "https://projectares.scm.azurewebsites.net/static/"
 
 TINYMCE_DEFAULT_CONFIG = {
     'height': 360,
