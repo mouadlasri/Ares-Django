1accf066a99b6940d3d01d38be347c734dd5fef2
Mouad <lasri.mouad98@gmail.com>
2019-08-22 00:20:17 +0100

    Added Azure host

--- Ares/settings.py
+++ Ares/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 PROJECT_DIR = os.path.dirname(__file__)
-STATIC_URL = '/static/'
-
+# STATIC_URL = '/static/'
+STATIC_URL = os.path.join(PROJECT_DIR,'/wwwroot/static/')
+# STATIC_ROOT = "/var/www/Ares.com/static/"
 # STATIC_ROOT = 'D:/home/site/wwwroot/static/'
 STATIC_ROOT= os.path.join(PROJECT_DIR,'/wwwroot/static/')
 # STATIC_ROOT = "https://projectares.scm.azurewebsites.net/static/"
